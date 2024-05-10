a61c4567865ff707b2f4d18a89514b5d9afa0c77
Ahmad Hassan <ahmadhassanyaseen@gmail.com>
2024-05-10 21:47:31 +0500

    Fixed Specil Rate Feature

--- addVehical.php
+++ addVehical.php
@@ -110,8 +110,165 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         if (isset($result_data['CURL_RESULT']) && $result_data['CURL_RESULT'] == "success") {
             $_SESSION['VNDR']['VEHICLES'][$result_data['id']]['vehicle'] = $result_data['id'];
             $multiselect_options = array('interior_style', 'onboard_luxury', 'media_capability', 'complimentary');
+            // print_r($_POST);
             foreach ($result_data as $rindx => $rdata) {
                 $_SESSION['VNDR']['VEHICLES'][$result_data['id']][$rindx] = $rdata;
+            }
+            // print_r($result_data['id']);
+            $vhdata['id'] = $result_data['id'];
+            $vhdata['method'] = 'fetchVehicleSpecialRatesIds';
+            $curl3 = curl_init($crm_url);
+            curl_setopt($curl3, CURLOPT_POST, true);
+            curl_setopt($curl3, CURLOPT_HEADER, false);
+            curl_setopt($curl3, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($curl3, CURLOPT_POSTFIELDS, $vhdata);
+            $response3 = curl_exec($curl3);
+
+
+            $result_data3 = json_decode($response3, true);
+            // print_r($_POST);
+            // echo "<br>";
+            // echo "<br>";
+            // print_r($result_data3);
+            $deletedItems = array();
+            $leftItems = array();
+
+
+            if ( $result_data3['data'] != "" && !empty($_POST['special_rate'])) {
+                // echo "lpo1";
+                $resultIds = array_column($result_data3['data'], 'id');
+
+                // Check for left items
+                foreach ($_POST['special_rate'] as $spVal) {
+                    if (in_array($spVal, $resultIds)) {
+                        $leftItems[] = $spVal;
+                    }
+                }
+
+                // Check for deleted items
+                foreach ($resultIds as $spRes) {
+                    if (!in_array($spRes, $_POST['special_rate'])) {
+                        $deletedItems[] = $spRes;
+                    }
+                }
+            } else {
+                // echo "lpo2";
+                if ( $result_data3['data'] != "") {
+                // echo "lpo3";
+                    $resultIds = array_column($result_data3['data'], 'id');
+                    // All items are deleted if there are no result data
+                    $deletedItems = $resultIds;
+                }
+            }
+
+
+
+            // ok
+
+            // Extracting IDs from $result_data3['data']
+            //             if(count($result_data3['data']) > 0){
+            //             $resultIds = array_column($result_data3['data'], 'id');
+            // // if($_POST['special_rate']){
+            //             // Checking which IDs are present in $_POST['special_rate']
+            //             foreach ($_POST['special_rate'] as $spVal) {
+            //                 if (in_array($spVal, $resultIds)) {
+            //                     $leftItems[] = $spVal;
+            //                 } 
+            //             }
+            //             foreach ($resultIds as $spRes) {
+            //                 if($_POST['special_rate']){
+            //                 foreach($_POST['special_rate'] as $spVal){
+            //                 if ($spVal !== $spRes) {
+            //                     $deletedItems[] = $spRes;
+            //                 } 
+            //             }
+            //             }else{
+            //                 $deletedItems[] = $spRes;
+            //             }
+            //             }
+            //         // }
+            //     }
+            // ok
+
+
+            // $result_data3 = json_decode($response3, true);
+            // print_r($result_data3['data']);
+            // $deletedItems = array();
+            // $leftItems = array();
+
+            // foreach ($result_data3['data'] as $result) {
+            //     foreach ($_POST['special_rate'] as $spIndx => $spVal) {
+            //         if ($result['id'] == $spVal) {
+            //             $leftItems[] = $spVal;
+            //             break; // Stop the loop once a match is found
+            //         }
+            //     }
+            // }
+
+            // // Iterate through the original data again to find deleted items
+            // foreach ($_POST['special_rate'] as $spIndx => $spVal) {
+            //     if (!in_array($spVal, $leftItems)) {
+            //         $deletedItems[] = $spVal;
+            //     }
+            // }
+
+            // print_r($response3);
+            // $result_data3 = json_decode($response3, true);
+            // print_r($result_data3['data']);
+            // $deletedItems = array();
+            // $leftItems = array();
+            // foreach ($result_data3['data'] as $result) {
+            //     // print_r($_POST['special_rate']);
+            //     // echo '<br>';
+            //     // print_r($result['vnd_special_rates_vnd_vechilesvnd_special_rates_idb']);
+            //     // // print_r($result);
+            //     // echo '<br>';
+
+            //     foreach ($_POST['special_rate'] as $spIndx => $spVal) {
+
+            //         if ($result['id'] == $spVal) {
+            //             $leftItems[] = $spVal;
+            //         }
+            //     }
+
+            // }
+            // foreach ($result_data3['data'] as $result) {
+            //     // print_r($_POST['special_rate']);
+            //     // echo '<br>';
+            //     // print_r($result['vnd_special_rates_vnd_vechilesvnd_special_rates_idb']);
+            //     // // print_r($result);
+            //     // echo '<br>';
+
+            //     foreach ($leftItems as $spIndx => $spVal) {
+
+            //         if ($result['id'] !== $spVal) {
+            //             $deletedItems[] = $spVal;
+            //         }
+
+            //     }
+
+            // }
+
+
+            // echo "<br>";
+            // echo "<br>";
+            // print_r($_POST);
+            // print_r($leftItems);
+            // echo "<br>";
+            // echo "<br>";
+            // print_r($deletedItems);
+            if($deletedItems && count($deletedItems) > 0){
+                foreach ($deletedItems as $delItem => $delVal) {
+                    $vhdata['id'] = $delVal;
+                    $vhdata['method'] = 'deleteSpecialRatesNew';
+                    $curl3 = curl_init($crm_url);
+                    curl_setopt($curl3, CURLOPT_POST, true);
+                    curl_setopt($curl3, CURLOPT_HEADER, false);
+                    curl_setopt($curl3, CURLOPT_RETURNTRANSFER, true);
+                    curl_setopt($curl3, CURLOPT_POSTFIELDS, $vhdata);
+                    $response3 = curl_exec($curl3);
+
+                }
             }
             $rates_object = array();
             $rates_mapping = array('name_special_rate', 'special_rate_start_from', 'special_rate_end_on', 'special_base_hourly_rate', 'special_base_min_hours', 'special_base_additional_hourly', 'custom_special_rates', 'special_hourly_rate_monday', 'special_hourly_rate_tuesday', 'special_hourly_rate_wednesday', 'special_hourly_rate_thursday', 'special_hourly_rate_friday', 'special_hourly_rate_saturday', 'special_hourly_rate_sunday', 'special_min_hours_monday', 'special_min_hours_tuesday', 'special_min_hours_wednesday', 'special_min_hours_thursday', 'special_min_hours_friday', 'special_min_hours_saturday', 'special_min_hours_sunday', 'special_additional_hourly_mon', 'special_additional_hourly_tue', 'special_additional_hourly_wed', 'special_additional_hourly_thu', 'special_additional_hourly_fri', 'special_additional_hourly_sat', 'special_additional_hourly_sun');
@@ -156,25 +313,32 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                 $rate_data['additional_hourly_saturday'] = trim($_POST['special_additional_hourly_sat'][$spIndx]);
                 $rate_data['additional_hourly_sunday'] = trim($_POST['special_additional_hourly_sun'][$spIndx]);
                 $rate_data['vnd_special_rates_vnd_vechilesvnd_vechiles_ida'] = $result_data['id'];
-                $rate_data['method'] = 'UpdateSepcialRate';
+                $rate_data['method'] = 'updateSpecialRatesNew';
+
+                // echo'<br>';
+
                 $curl2 = curl_init($crm_url);
                 curl_setopt($curl2, CURLOPT_POST, true);
                 curl_setopt($curl2, CURLOPT_HEADER, false);
                 curl_setopt($curl2, CURLOPT_RETURNTRANSFER, true);
                 curl_setopt($curl2, CURLOPT_POSTFIELDS, $rate_data);
                 $response = curl_exec($curl2);
+                // print_r($rate_data);
+                // print_r($response);
+                // echo '<br>';
+                // echo '<br>';
                 if ($response == 'error') {
                     $errors[] = 'Error occurred while adding special rate';
                 } else if ($response == 'skip') {
                 } else {
-                    $rates_object[$response]['special_rate'] = $response;
-                    foreach ($rates_mapping as $rIndx)
-                        $rates_object[$response][$rIndx] = trim($_POST[$rIndx][$spIndx]);
-                    $rates_object[$response]['special_rate'] = trim($_POST['special_rate'][$spIndx]);
+                    // $rates_object[$response]['special_rate'] = $response;
+                    // foreach ($rates_mapping as $rIndx)
+                    //     $rates_object[$response][$rIndx] = trim($_POST[$rIndx][$spIndx]);
+                    // $rates_object[$response]['special_rate'] = trim($_POST['special_rate'][$spIndx]);
                 }
             }
-            $_SESSION['VNDR']['VEHICLES'][$result_data['id']]['VEHICLESRATES'] = $rates_object;
-            header("Location:" . $redirects['profile_complete']);
+            // $_SESSION['VNDR']['VEHICLES'][$result_data['id']]['VEHICLESRATES'] = $rates_object;
+            header("Location: http://localhost/vendor/vehicle.php" );
         } else {
             $errors[] = $response;
         }
@@ -183,7 +347,21 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 if (isset($_REQUEST['vehicle']) && !empty($_REQUEST['vehicle'])) {
     if (isset($_SESSION['VNDR']['VEHICLES'][$_REQUEST['vehicle']])) {
         $vehicle_object = $_SESSION['VNDR']['VEHICLES'][$_REQUEST['vehicle']];
-        $vehicle_object['SpeicalRates'] = $_SESSION['VNDR']['VEHICLES'][$_REQUEST['vehicle']]['VEHICLESRATES'];
+        // $vehicle_object['SpeicalRates'] = $_SESSION['VNDR']['VEHICLES'][$_REQUEST['vehicle']]['VEHICLESRATES'];
+
+
+        $data['id'] = $vehicle_object['vehicle'];
+        $data["method"] = "fetchSpecialRatesNew";
+        $curl = curl_init($crm_url);
+        curl_setopt($curl, CURLOPT_POST, true);
+        curl_setopt($curl, CURLOPT_HEADER, false);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+        $response = curl_exec($curl);
+        // print_r($response);
+        $specialRates = json_decode($response, true);
+        $vehicle_object['SpeicalRates'] = $specialRates['data'];
+        $vehicle_object['xeno'] = "ahmad";
     } else {
         $data = array();
         $data = $_POST;
